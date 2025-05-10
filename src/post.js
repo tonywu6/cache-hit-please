6261a366206773ebd48934fc37409853dca768a5
Tony Wu <tonywu6@protonmail.com>
2025-05-10 22:10:38 +0800

    GH_TOKEN

--- src/post.js
+++ src/post.js
@@ -1,5 +1,3 @@
-import process from "node:process";
-
 import { saveCache } from "@actions/cache";
 import { getState, warning } from "@actions/core";
 import { exec } from "@actions/exec";
@@ -11,12 +9,8 @@ tryCatch(async () => {
 
   if (getState("cache-hit") === "true") {
     try {
-      await exec("gh", ["cache", "delete", keys[0]], {
-        env: {
-          ...process.env,
-          GH_TOKEN: String(process.env.GITHUB_TOKEN),
-        },
-      });
+      const env = { ...process.env, GH_TOKEN: process.env.GITHUB_TOKEN || "" };
+      await exec("gh", ["cache", "delete", keys[0]], { env });
     } catch (e) {
       warning(`could not evict cache: ${e}`);
     }
